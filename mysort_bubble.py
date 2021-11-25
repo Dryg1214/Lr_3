32af84a78f346fc1398b7db9f2dd5f49fdfe64e5
Dryg1214 <pakazancev901@gmail.com>
2021-11-25 05:45:35 +0400

    Не крутой прогресс бар

--- mysort_bubble.py
+++ mysort_bubble.py
@@ -1,11 +1,11 @@
-
+from tqdm import tqdm
 # вес паспорт снилс возраст
 def bubble_sort(lst, name_field):
     n = len(lst)
     check = True
     while check:
         check = False
-        for i in range(n-1):
+        for i in tqdm(range(n-1)):
             if lst[i][name_field] > lst[i + 1][name_field]:
                 c = lst[i]
                 lst[i] = lst[i + 1]
